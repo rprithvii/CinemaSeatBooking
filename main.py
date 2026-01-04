efe95e242a28a48c5ad6307f63a694f004e10245
Prithvi Raj <mail.prithvi@gmail.com>
2026-01-04 19:56:46 +0000

    Added another book_ticket step in main.py and made the Cinema grid 5x5

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from src.cinema import Cinema
 
-c = Cinema(3,3)
+c = Cinema(5,5)
 c.display_cinema()
 
 c.book_ticket(2,2)
 c.display_cinema()
 
 c.book_ticket(1,3)
+c.display_cinema()
+
+c.book_ticket(3,3)
 c.display_cinema()
